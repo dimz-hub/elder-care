8348a634d994e062dacd453e8f04f049e2088c6a
olawale oladimeji <oladimejiolawale1999@gmail.com>
2024-01-24 14:16:32 +0100

    contact fade

--- src/Components/ContactPage.jsx
+++ src/Components/ContactPage.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import Navbar from './Navbar'
 import Contact from './Contact'
 import Footer from './Footer'
 
-export default function ContactPage({contactRef}) {
+export default function ContactPage() {
+
+  const useIntersectionObserver = (callback, options = {}) => {
+    const targetRef = useRef(null);
+ 
+    useEffect(() => {
+      const observer = new IntersectionObserver(callback, options);
+      const targetElement = targetRef.current;
+ 
+      if (targetElement) {
+        observer.observe(targetElement);
+      }
+ 
+      return () => {
+        if (targetElement) {
+         observer.unobserve(targetElement);
+        }
+      };
+    }, [callback, options]);
+ 
+    return targetRef;
+  };
+
+
+
+  const contactRef = useIntersectionObserver((entries) => handleIntersection(entries, 'contact'), { threshold: 0.5 });
+
+const handleIntersection = (entries, section) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+       // Add your animation logic for the specific section here
+      const element = entry.target;
+        element.style.opacity = 1;
+       //  element.classList.add('fadeInAnimation');
+     }
+    });
+  };
+
   return (
     <div>
 <div className='bg-[#7FC7D9]'>
