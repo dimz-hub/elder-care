aefc4f042e26e77f88491abdfa67f66d40f154b5
olawale oladimeji <oladimejiolawale1999@gmail.com>
2024-02-09 13:48:25 +0100

    blur background

--- src/Components/TopPage.jsx
+++ src/Components/TopPage.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Navbar from './Navbar'
 import Hero from './Hero'
 
 export default function TopPage({scrolled, topRef}) {
 
-
+const  [loaded, setLoaded] = useState(false)
   
+ function handleImageLoad () {
+  setLoaded(true)
+ }
+
   return (
     <div  className='toppage'>
 
-    <div className='hero h-[100vh] w-[100vw]'>
-      <div className='hero-container xs:z-20'>
+    <div className={`hero h-[100vh] w-[100vw] ${loaded? 'loaded' : ''}  `}>
+      <img 
+      onLoad={handleImageLoad}
+      src='images/elder-sunset.jpg'
+      style={{display:'none'}}
+      />
+      <div className='content hero-container xs:z-20'>
+
         <div  className='fixed top-0 left-0 navbar-main xs:absolute'>
 
         <Navbar scrolled = {scrolled}  />
