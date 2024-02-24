9d29ecfdb5e626cf2e5469a50c28a84bcbed0775
olawale oladimeji <oladimejiolawale1999@gmail.com>
2024-02-24 22:24:36 +0100

    contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -15,30 +15,30 @@ export default function Contact({contactRef}) {
 
   return (
     <div className='w-[100vw] h-[100vh] mission flex items-center justify-center  xs:min-h-[100vh] xs:h-[auto] md:h-[auto] md:pb-[30px] lg:h-[auto] lg:pb-[30px]' >
-        <div className='w-[90vw] text-[] m-[auto] xs:w-[100vw]   ' ref={contactRef} style={{ opacity: 0, transition: 'opacity 1s ease-in' }}>
+        <div className='w-[90v] m-[auto] xs:w-[90vw]    ' ref={contactRef} style={{ opacity: 0, transition: 'opacity 1s ease-in' }}>
           <div className='flex items-center justify-center gap-[50px] xs:flex-col  md:flex-col lg:flex-col'>
 
             <div className='xs:text-center xs:flex xs:flex-col xs:justify-center xs:items-center '>
           <h3 className='font-[600] text-[50px] xs:text-[30px] xs:mb-[30px] text-[#4F6F52]'>Get in touch with us</h3>
-          <div className='xs:flex xs:flex-col xs:gap-[10px] xs:pl-[15px]'>
-            <div className='flex gap-[20px] items-center xs:text-center xs:w-[100vw]  xs:gap-[10px] xs:items-center '>
-                <div className='font-[500] text-[40px]  xs:text-[18px]' >Numbers:</div>
-                <div className='flex items-center gap-[8px] mt-[10px] xs:mt-[5px]'>
+          <div className='xs:flex xs:flex-col xs:gap-[10px] xs:pl-[]'>
+            <div className='flex   gap-[20px] items-center xs:text-center xs:w-[100vw]  xs:gap-[10px] xs:items-center  xs:flex-col'>
+                <div className='font-[500] text-[40px]  xs:text-[25px] xs:font-[600]' >Numbers<span className='xs:hidden'>:</span></div>
+                <div className='flex items-center gap-[8px] mt-[10px] xs:mt-[5px] xs:flex-col xs:gap-[5px] '>
  
-            <p className='text-[20px]  font-[600] xs:text-[14px] '>07597936369</p>
-            <p className='text-[20px]  font-[600] xs:text-[14px]'>07724537007</p>
+            <p className='text-[20px]  font-[600] xs:text-[19px] '>07597936369</p>
+            <p className='text-[20px]  font-[600] xs:text-[19px]'>07724537007</p>
                 </div>
             </div>
-            <div className='flex items-center gap-[20px]'>
-                <div className='font-[500] text-[40px] xs:text-[18px]'   >Emails:</div>
-                <div className='flex gap-[10px] items-center mt-[10px]  xs:mt-[5px]'>
+            <div className='flex items-center gap-[20px]  xs:flex-col xs:gap-[10px]'>
+                <div className='font-[500] text-[40px] xs:text-[25px]  xs:font-[600]'>Emails<span className='xs:hidden' >:</span></div>
+                <div className='flex gap-[10px] items-center mt-[10px] xs:gap-[5px] xs:mt-[5px] xs:flex-col'>
 
-            <p className='text-[20px] font-[500] xs:text-[14px]'>Sade@joinbirch.org</p>
-            <p className='text-[20px] font-[500] xs:text-[14px]'>Tiffany@joinbirch.org </p>
+            <p className='text-[20px] font-[500] xs:text-[19px]'>Sade@joinbirch.org</p>
+            <p className='text-[20px] font-[500] xs:text-[19px]'>Tiffany@joinbirch.org </p>
                 </div>
             </div>
-            <div className='flex items-center gap-[20px]'>
-                <div className='font-[500] text-[40px] xs:text-[18px]' >Socials:</div>
+            <div className='flex items-center gap-[20px] xs:flex-col xs:gap-[10px]' >
+                <div className='font-[500] text-[40px] xs:text-[25px] xs:font-[600]' >Socials<span className='xs:hidden'>:</span></div>
                 <div className='flex items-center gap-[10px] jusify-center'>
 
                   <div onClick={redirectToFacebook} className='cursor-pointer'>
